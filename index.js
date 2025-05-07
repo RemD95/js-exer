dd0eb07a1c6ed79b4aed0ce1761bc40b26bd6b91
Remus Daniel Blaj <remoblaj@gmail.com>
2025-05-08 00:12:32 +0200

    ridefinizione e scoping delle variabili

--- index.js
+++ index.js
@@ -3,7 +3,22 @@ let valore = 1;
 const value= 2;
 console.log(valore,value);
 
-let val = 5
-console.log(val)
-val = 8
-console.log(val)
+let val = 5;
+console.log(val);
+val = 8;
+console.log(val);
+
+let number = 10;
+console.log(number);
+{
+    let number = 9;
+    console.log(number);
+}
+let outside = 3;
+{
+    let inside = 4;
+    console.log(outside);
+console.log(inside);
+}
+console.log(outside);
+console.log(inside)
